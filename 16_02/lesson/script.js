c17232e0738a4e217876c7f77a586a3f2f335cdb
Aizhan <aizh.aidarova@gmail.com>
2022-02-16 10:33:41 +0600

    feat: start homework

---
+++ 16_02/lesson/script.js
@@ -0,0 +1,105 @@
+/*
+let name1 = 'Aizhan';
+let name2 = 'Aidarova';
+name2 = name1;
+alert(name2)
+*/
+
+
+
+                                     /******************** Типы данных ********************/
+
+
+/******************** Число(number) ********************/
+
+/*
+let age = 66;
+console.log(age);
+
+// infinity
+let result = 5 / 0;
+console.log(result);
+
+// Nan - Not a Number
+let result2 = 'Hello' * 5;
+console.log(result2);
+*/
+
+/******************** Строка(string) ********************/
+
+/*
+let str1 = 'HTML';
+let str2 = 'CSS';
+let str3 = `Javascript ${str1} ${str2} ${new Date().toLocaleString()}`;
+let str4 = 'Javascript ' + str1 + ' ' + str2 + ' ' + new Date().toLocaleString() + '.';
+console.log(str1);
+console.log(str2);
+console.log(str3);
+console.log(str4);
+*/
+
+/******************** Логический(boolean) ********************/
+
+/*
+let isMan = true;
+let isProgrammer = false;
+
+let result = 5 > 6;
+console.log(result);
+*/
+
+/******************** Неизвестно(null) ********************/
+
+/*let name = null;
+console.log(name);*/
+
+/******************** Undefined ********************/
+
+/*let nameData;
+console.log(nameData);*/
+
+/******************** Object ********************/
+
+/*
+const userInfo = {
+    name:'Maks',
+    age:30
+};
+console.log(userInfo);
+*/
+
+/******************** Symbols Object ********************/
+
+
+/******************** BigInt ********************/
+
+/*let num = 1234567890123456n;
+console.log(num)*/
+
+// let queen = result;
+// console.log(typeof queen)
+
+let num = 9;
+console.log(typeof (num));
+
+let str ='Hello';
+console.log(typeof (str));
+
+let result = 7 / 0;
+console.log(typeof (result));
+
+let isGirl = true;
+let isProgrammer = false;
+console.log(typeof (isGirl));
+
+let name = null;
+console.log(typeof (name));
+
+const userInfo = {
+    name: 'queen',
+    age: 88
+};
+console.log(typeof (userInfo));
+
+
+
