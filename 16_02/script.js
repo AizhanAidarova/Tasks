102b8852421a81ab6b38abafaaab76edaaa9991b
Aizhan <aizh.aidarova@gmail.com>
2022-02-16 11:19:24 +0600

    feat: finish homework

---
+++ 16_02/script.js
@@ -0,0 +1,14 @@
+/*********************************Первое задание************************************/
+
+let camelCase = 1;
+let snake_case = 2;
+let PascalCase = 3;
+alert (`${camelCase} ${snake_case} ${PascalCase}`)
+console.log (`${camelCase} ${snake_case} ${PascalCase}`)
+
+/*********************************Второе задание************************************/
+
+let str2 = 'Бишкек!';
+let str1 = 'Салам,' + " " + str2;
+alert (str1);
+console.log (str1)
