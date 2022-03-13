952df85d18f18219f285fae1fb749fd6800e1b42
Aizhan <aizh.aidarova@gmail.com>
2022-03-13 14:33:33 +0600

    hw 11-03

---
+++ 11_03_22/form1/script.js
@@ -0,0 +1,35 @@
+let button = document.querySelector('#button');
+
+button.onclick = (event) => {
+    event.preventDefault();
+    let userlastName = document.querySelector('#user-lastName').value;
+    let userName = document.querySelector('#user-lastName').value;
+    let userPatronymic = document.querySelector('#user-patronymic').value;
+    let userDate = document.querySelector('#user-date').value;
+    let userAddress = document.querySelector('#user-address').value;
+    let userSerialNumber = document.querySelector('#user-serialNumber').value;
+    let userInn = document.querySelector('#user-inn').value;
+    let userDateOfReceipt = document.querySelector('#user-dateOfReceipt').value;
+    let userContactNumber = document.querySelector('#user-contactNumber').value;
+    let userMail = document.querySelector('#user-mail').value;
+    let userNameOfBank = document.querySelector('#user-nameOfBank').value;
+    let userFactAddress = document.querySelector('#user-factAddress').value;
+
+
+    let object = {
+        lastname: userlastName,
+        name: userName,
+        patronymic: userPatronymic,
+        date: userDate,
+        address: userAddress,
+        serialNumber: userSerialNumber,
+        inn: userInn,
+        dateOfReceipt: userDateOfReceipt,
+        contactNumber: userContactNumber,
+        mail: userMail,
+        banks: userNameOfBank,
+        factAddress: userFactAddress
+    }
+    console.log(object)
+
+}
