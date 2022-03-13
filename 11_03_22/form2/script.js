9da29a8398b319c5df804f880240ef146879b693
Aizhan <aizh.aidarova@gmail.com>
2022-03-13 16:28:48 +0600

    hw 11-03 form2

---
+++ 11_03_22/form2/script.js
@@ -0,0 +1,33 @@
+let button = document.querySelector('#button');
+
+button.onclick = (event) =>{
+    event.preventDefault()
+    let company = document.querySelector('#company').value;
+    let position = document.querySelector('#position').value;
+    let earn = document.querySelector('#earn').value;
+    let earn3Month = document.querySelector('#earn3Month').value;
+    let thing = document.querySelector('#thing').value;
+    let price = document.querySelector('#price').value;
+    let amount = document.querySelector('#amount').value;
+    let total = document.querySelector('#total').value;
+    let credit = document.querySelector('#credit').value;
+    let credit3m = document.querySelector('#credit3m').value;
+    let address = document.querySelector('#address').value;
+
+    let object2 = {
+        name:company,
+        position:position,
+        salary:earn,
+        salaryFor3Month:earn3Month,
+        thing:thing,
+        price:price,
+        amount:amount,
+        total:total,
+        credit:credit,
+        credit3m:credit3m,
+        address:address
+    }
+    console.log(object2);
+
+
+}
